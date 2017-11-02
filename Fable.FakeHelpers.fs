9ae91acc18029e2e90a196555a8ea416c20b04cd
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2017-11-02 16:50:43 +0100

    Moar helpers

--- Fable.FakeHelpers.fs
+++ Fable.FakeHelpers.fs
@@ -6,6 +6,10 @@ open System.Text.RegularExpressions
 open Fake
 open Fake.ReleaseNotesHelper
 
+let [<Literal>] RELEASE_NOTES = "RELEASE_NOTES.md"
+let NUGET_VERSION = Regex("<Version>(.*?)</Version>", RegexOptions.IgnoreCase)
+let NPM_VERSION = Regex(@"""version"":\s*""(.*?)""")
+
 let visitFile (visitor: string->string) (fileName : string) =
     // This code is supposed to prevent OutOfMemory exceptions
     // but it outputs wrong BOM
@@ -56,11 +60,26 @@ let run workingDir fileName args =
     if not ok then
         failwithf "'%s> %s %s' task failed" workingDir fileName args
 
-let needsPublishing (versionRegex: Regex) (releaseNotes: ReleaseNotes) projFile =
-    printfn "Project: %s" projFile
+let loadReleaseNotes projFile =
+    let projDir =
+        if Path.HasExtension(projFile)
+        then Path.GetDirectoryName(projFile)
+        else projFile
+    findFileUpwards RELEASE_NOTES projDir
+    |> ReleaseNotesHelper.LoadReleaseNotes
+
+let needsPublishing silent (versionRegex: Regex) (releaseNotes: ReleaseNotes) projFile =
+    let print msg =
+        if not silent then
+            let projName =
+                let projName = Path.GetFileNameWithoutExtension(projFile)
+                if projName = "package" // package.json
+                then Path.GetFileName(Path.GetDirectoryName(projFile))
+                else projName
+            printfn "%s > %s" projName msg
     if releaseNotes.NugetVersion.ToUpper().EndsWith("NEXT")
     then
-        printfn "Version in Release Notes ends with NEXT, don't publish yet."
+        print "Version in Release Notes ends with NEXT, don't publish yet."
         false
     else
         File.ReadLines(projFile)
@@ -68,18 +87,16 @@ let needsPublishing (versionRegex: Regex) (releaseNotes: ReleaseNotes) projFile 
             let m = versionRegex.Match(line)
             if m.Success then Some m else None)
         |> function
-            | None -> failwith "Couldn't find version in project file"
+            | None -> failwithf "Couldn't find version in %s" projFile
             | Some m ->
                 let sameVersion = m.Groups.[1].Value = releaseNotes.NugetVersion
                 if sameVersion then
-                    printfn "Already version %s, no need to publish." releaseNotes.NugetVersion
+                    sprintf "Already version %s, no need to publish" releaseNotes.NugetVersion |> print
                 not sameVersion
 
-let pushNuget dotnetExePath (releaseNotes: ReleaseNotes) (projFiles: string list) =
-    let versionRegex = Regex("<Version>(.*?)</Version>", RegexOptions.IgnoreCase)
-    projFiles
-    |> Seq.filter (needsPublishing versionRegex releaseNotes)
-    |> Seq.iter (fun projFile ->
+let pushNuget dotnetExePath (releaseNotes: ReleaseNotes) build (projFile: string) =
+    if needsPublishing false NUGET_VERSION releaseNotes projFile then
+        build |> Option.iter (fun build -> build())
         let projDir = Path.GetDirectoryName(projFile)
         let nugetKey =
             match environVarOrNone "NUGET_KEY" with
@@ -88,8 +105,8 @@ let pushNuget dotnetExePath (releaseNotes: ReleaseNotes) (projFiles: string list
         // Restore dependencies here so they're updated to latest project versions
         run projDir dotnetExePath "restore"
         // Update the project file
-        (versionRegex, projFile) ||> replaceLines (fun line _ ->
-            versionRegex.Replace(line, "<Version>"+releaseNotes.NugetVersion+"</Version>") |> Some)
+        (NUGET_VERSION, projFile) ||> replaceLines (fun line _ ->
+            NUGET_VERSION.Replace(line, "<Version>"+releaseNotes.NugetVersion+"</Version>") |> Some)
         try
             let tempDir = projDir </> "temp"
             CleanDir tempDir
@@ -104,25 +121,41 @@ let pushNuget dotnetExePath (releaseNotes: ReleaseNotes) (projFiles: string list
             |> printfn "There's been an error when pushing project: %s"
             printfn "Please revert the version change in .fsproj"
             reraise()
-    )
 
-let publishPackages baseDir dotnetExePath (packages: string list) =
-    for pkg in packages do
+let pushNpm (releaseNotes: ReleaseNotes) build (pkgJson: string) =
+    let projDir = Path.GetDirectoryName(pkgJson)
+    if needsPublishing false NPM_VERSION releaseNotes pkgJson then
+        build |> Option.iter (fun build -> build())
+        run projDir "npm" ("version " + releaseNotes.NugetVersion)
+        try
+            let publishCmd =
+                if releaseNotes.NugetVersion.IndexOf("-") > 0
+                then "publish --tag next"
+                else "publish"
+            run projDir "npm" publishCmd
+        with _ ->
+            printfn "There's been an error when pushing project: %s" projDir
+            printfn "Please revert the version change in package.json"
+            reraise()
+
+let publishPackages2 baseDir dotnetExePath (packages: (_*string) list) =
+    for f, pkg in packages do
         let fsProj, npmProj =
             if pkg.EndsWith(".fsproj")
             then baseDir </> pkg, None
             else baseDir </> (pkg + ".fsproj"), Some (baseDir </> pkg </> "package.json")
         if File.Exists(fsProj) then
-            let projDir = Path.GetDirectoryName(fsProj)
-            let release =
-                findFileUpwards "RELEASE_NOTES.md" projDir
-                |> ReleaseNotesHelper.LoadReleaseNotes
-            pushNuget dotnetExePath release [fsProj]
+            pushNuget dotnetExePath (loadReleaseNotes fsProj) f fsProj
         else
             match npmProj with
             | Some npmProj ->
                 if File.Exists(npmProj)
-                then failwith "TODO: push npm package"
+                then pushNpm (loadReleaseNotes npmProj) f npmProj
                 else failwithf "Couldn't find %s nor %s" fsProj npmProj
             | None ->
                 failwithf "Couldn't find %s" fsProj
+
+let publishPackages baseDir dotnetExePath packages =
+    packages
+    |> List.map (fun x -> None, x)
+    |> publishPackages2 baseDir dotnetExePath
